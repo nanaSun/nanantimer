fd7a0fb0f979f769be5f4263e58c82723907cc27
cherryvenus <1128yeye@sina.com>
2019-01-10 18:14:00 +0800

    more detail

--- test/test.js
+++ test/test.js
@@ -4,60 +4,92 @@ const {runIntervalTimer,runTimer}=require("../time")
 const chai=require("chai")
 const expect=chai.expect
 const assert=chai.assert
-describe('runIntervalTimer', function() {
-    describe(`#single runIntervalTimer() with burden ande less burden`, function() {
-        it('less burdened', function(done) {
-            runIntervalTimer("a",100,function(){},1000,function(usedTime,counts){
-                console.log(counts);
-                assert.ok(true);
-                done();
-            }).starttimer()
-        });
-        it('more burdened', function(done) {
-            runIntervalTimer("a",100,function(){
-                let array=[]
-                for(let j=0;j<10000000;j++){
-                    array.push(j)
-                }
-            },1000,function(usedTime,counts){
-                console.log(counts);
-                assert.ok(true);
-                done();
-            }).starttimer()
-        });
-    })
-    describe(`#multi runIntervalTimer() running at the same time`, function() {
-        it('multi runIntervalTimer with no burden', function(done) {
-            let countdown=0
-            for(let i=0;i<10;i++){
-                runIntervalTimer(i,100,function(){},1000,function(usedTime,counts,id){
-                    console.log(id,counts)
-                    expect(countdown).to.be.equal(id);
-                    countdown++
-                    if(countdown>=10) done()
-                }).starttimer()
-            }
-        });
-        it('multi runIntervalTimer with burden', function(done) {
-            let countdown=0
-            for(let i=0;i<10;i++){
-                runIntervalTimer(i,100,function(){
-                    let array=[]
-                    for(let j=0;j<10000000;j++){
-                        array.push(j)
-                    }
-                },1000,function(usedTime,counts,id){
-                    console.log(`id${counts}`)
-                    assert.ok(true);
-                    countdown++
-                    if(countdown>=10) done()
-                }).starttimer()
-            }
-        });
-    })
-});
+// describe('runIntervalTimer', function() {
+//     describe(`#single runIntervalTimer() with burden ande less burden`, function() {
+//         it('less burdened', function(done) {
+//             runIntervalTimer("a",100,function(){},1000,function(usedTime,counts){
+//                 console.log(counts);
+//                 assert.ok(true);
+//                 done();
+//             }).starttimer()
+//         });
+//         it('more burdened', function(done) {
+//             runIntervalTimer("a",100,function(){
+//                 let array=[]
+//                 for(let j=0;j<10000000;j++){
+//                     array.push(j)
+//                 }
+//             },1000,function(usedTime,counts){
+//                 console.log(counts);
+//                 assert.ok(true);
+//                 done();
+//             }).starttimer()
+//         });
+//     })
+//     describe(`#multi runIntervalTimer() running at the same time`, function() {
+//         it('multi runIntervalTimer with no burden', function(done) {
+//             let countdown=0
+//             for(let i=0;i<10;i++){
+//                 runIntervalTimer(i,100,function(){},1000,function(usedTime,counts,id){
+//                     console.log(id,counts)
+//                     expect(countdown).to.be.equal(id);
+//                     countdown++
+//                     if(countdown>=10) done()
+//                 }).starttimer()
+//             }
+//         });
+//         it('multi runIntervalTimer with burden', function(done) {
+//             let countdown=0
+//             for(let i=0;i<10;i++){
+//                 runIntervalTimer(i,100,function(){
+//                     let array=[]
+//                     for(let j=0;j<10000000;j++){
+//                         array.push(j)
+//                     }
+//                 },1000,function(usedTime,counts,id){
+//                     console.log(`id${counts}`)
+//                     assert.ok(true);
+//                     countdown++
+//                     if(countdown>=10) done()
+//                 }).starttimer()
+//             }
+//         });
+//     })
+// });
 
 describe('runTimer', function() {
+    describe(`#single runTimer() stop at any time`, function() {
+        it(' stop at any time', function(done) {
+            let timer;
+            let stop=false
+            setTimeout(()=>{
+                stop=true
+            },200)
+            timer=runTimer("a",100,function(id,runtime,counts){
+                if(stop){
+                   timer.cleartimer()
+                }
+            },1000,function(id,usedTime,counts){
+                expect(usedTime).to.be.lessThan(300)
+                done();
+            })
+            timer.starttimer()
+        });
+    });
+    describe(`#single runTimer() stop at any counts`, function() {
+        it(' stop at any counts', function(done) {
+            let timer;
+            timer=runTimer("a",100,function(id,runtime,counts){
+                if(counts===2){
+                   timer.cleartimer()
+                }
+            },1000,function(id,usedTime,counts){
+                expect(counts).to.be.equal(2)
+                done();
+            })
+            timer.starttimer()
+        });
+    });
     describe(`#single runTimer() with burden ande less burden`, function() {
         it('less burdened', function(done) {
             runTimer("a",100,function(){},1000,function(usedTime,counts){
--- time.js
+++ time.js
@@ -1,47 +1,59 @@
-//setTimeout版本
+/*
+id：定时器id，自定义
+aminTime：执行间隔时间
+callback：定时执行的函数，返回callback(id,runtime)，id是定时器的时间，runtime是当前运行的时间
+maxTime：定时器重复执行的最大时长
+afterTimeUp：定时器超时之后的回调函数，返回afterTimeUp(id,usedTime,countTimes)，id是定时器的时间，usedTime是定时器执行的总时间，countTimes是当前定时器运行的回调次数
+*/
 function runTimer(id,aminTime,callback,maxTime,afterTimeUp){
-    let timer=null
-    let running=false
-    let usedTime=0
-    let startTime=0
-    let countTimes=0
-    function getTime(){
+    let timer=null//当前定时器的系统中的id，用于clearTimeout的执行
+    let running=false//防止多次执行
+    let usedTime=0//耗时时间
+    let startTime=0//开始时间
+    let countTimes=0//一共执行了几次
+    function getTime(){//获取当前时间
         return new Date().getTime();
     }
-    function timeout(diffTime){
+    /*
+    diffTime：需要扣除的时间
+    */
+    function timeout(diffTime){//主要函数，定时器本体
         if(!running) return
-        let runtime=aminTime-diffTime
+        let runtime=aminTime-diffTime//计算下一次的执行间隔
+        if(getTime()-usedTime>=maxTime){ //超出了最大时间，清空当前定时器并返回
+            cleartimer()
+            return
+        }
         timer=setTimeout(()=>{
-            if(getTime()-usedTime>=maxTime){ 
+            if(getTime()-usedTime>=maxTime){//超出了最大时间，清空当前定时器并返回
                 cleartimer()
                 return
             }
             countTimes++
+            //计算需扣除的时间，并执行下一次的调用
             let tmp=startTime
-            callback(runtime);
+            callback(id,runtime,countTimes);
             startTime=getTime()
             diffTime=(startTime-tmp)-aminTime
             timeout(diffTime)
         },runtime)
     }
-    function cleartimer(){
+    function cleartimer(){//结束定时器
         running=false
         usedTime=getTime()-usedTime
-        afterTimeUp(usedTime,countTimes,id)
+        afterTimeUp(id,usedTime,countTimes)
         clearTimeout(timer);
         //timer=null
     }
-    function starttimer(){
+    function starttimer(){//开始定时器
         running=true
         startTime=getTime()
         usedTime=startTime
         timeout(0)
     }
+    //f返回开始和结束这两个控制的方法
     return {cleartimer,starttimer}
-    
 }
-
-
 //setTimeInterVal版本
 function runIntervalTimer(id,aminTime,callback,maxTime,afterTimeUp){
     let timer=null
